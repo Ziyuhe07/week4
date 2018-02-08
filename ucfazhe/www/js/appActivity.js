0001e87588fe85a61d7a42d7bc80772abaea22f6
Ziyuhe07 <ucfazhe@ucl.ac.uk>
2018-02-08 12:23:30 -0500

    LeafletMap

--- ucfazhe/www/js/appActivity.js
+++ ucfazhe/www/js/appActivity.js
@@ -6,6 +6,21 @@ attribution: 'Map data &copy; <ahref="http://openstreetmap.org">OpenStreetMap</a
 'Imagery © <a href="http://mapbox.com">Mapbox</a>',
 id: 'mapbox.streets'
 }).addTo(mymap);
+L.circle([51.508, -0.11], 500, {
+			color: 'red',
+			fillColor: '#f03',
+			fillOpacity: 0.5
+			}).addTo(mymap).bindPopup("I am a circle.");
+		
+var myPolygon = L.polygon([
+						   [51.509, -0.08],
+						   [51.503, -0.06],
+						   [51.51, -0.047]
+			                ],{
+								color: 'red',
+								fillColor: '#f03',
+								fillOpacity: 0.5
+								}).addTo(mymap).bindPopup("I am a polygon.");
 var geojsonFeature = {
  "type": "Feature",
  "properties": {
